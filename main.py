9b1ca57428f496d2f55b859a66834f6013355b89
Roxanna <112521175+Rvxannaduran@users.noreply.github.com>
2022-11-07 21:26:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import requests
 import private
 import redis
 
-r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 app = Flask(__name__)
 url = "https://hooks.slack.com/services/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"
